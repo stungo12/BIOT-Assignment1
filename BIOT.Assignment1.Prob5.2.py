8e91dce756c60e93ef72ac32838b769b84e78946
stungo12 <123669479+stungo12@users.noreply.github.com>
2023-03-05 20:14:56 -0600

    Add files via upload

---
+++ BIOT.Assignment1.Prob5.2.py
@@ -0,0 +1,16 @@
+# Names: Brian, Dylan, and Matthew
+
+# Python program that compares two strings to see if they are equal or not. 
+
+# A pair of strings 
+x = "ATGGGGA"
+y = "ATGCCA"
+
+# compare x and y
+if (x != y): # If x and y are the same then print a statement that they are equal
+    print("x and y are not equal.")
+    
+# Else, if they are not equal, then say that x and y are not equal
+else:
+    print("x and y are equal.")
+        
